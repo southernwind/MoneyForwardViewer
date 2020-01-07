4e8f98bcc28af0b5878f143862cee3f5700d9995
southernwind <testakina@gmail.com>
2020-01-07 15:11:30 +0900

    支出を円グラフにして表示

--- MoneyForwardViewer/ViewModels/MainWindowViewModel.cs
+++ MoneyForwardViewer/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,11 @@ using Prism.Mvvm;
 
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using System;
+using OxyPlot;
+using System.Reactive.Linq;
+using OxyPlot.Series;
+using System.Linq;
 
 namespace MoneyForwardViewer.ViewModels {
 	internal class MainWindowViewModel : BindableBase {
@@ -21,6 +26,10 @@ namespace MoneyForwardViewer.ViewModels {
 			get;
 		}
 
+		public IReadOnlyReactiveProperty<PlotModel> LargeCategoryModel {
+			get;
+		}
+
 		public ReactiveCommand ImportCommand {
 			get;
 		} = new ReactiveCommand();
@@ -33,6 +42,24 @@ namespace MoneyForwardViewer.ViewModels {
 			this.Id = moneyForward.Id.ToReactivePropertyAsSynchronized(x => x.Value);
 			this.Password = moneyForward.Password.ToReactivePropertyAsSynchronized(x => x.Value);
 			this.Transactions = moneyForward.Transactions.ToReadOnlyReactivePropertySlim();
+			this.LargeCategoryModel = this.Transactions.Select(x => {
+				var pm = new PlotModel {
+					Title = "支出"
+				};
+				var pie = new PieSeries();
+				pie.TrackerFormatString = "{1}: \\{2:#,0} ({3:P1})";
+				var slices =
+					x.Where(t => t.IsCalculateTarget)
+						.GroupBy(t => t.LargeCategory)
+						.Select(g => new PieSlice(g.Key, g.Sum(x => -x.Amount)))
+						.Where(x => x.Value > 0)
+						.OrderBy(x => x.Value);
+				foreach (var slice in slices) {
+					pie.Slices.Add(slice);
+				}
+				pm.Series.Add(pie);
+				return pm;
+			}).ToReadOnlyReactivePropertySlim();
 			this.ImportCommand.Subscribe(async () => {
 				await moneyForward.ImportFromMoneyForward();
 			});
